46729c4e617425b27f5b6d4307016be7de487609
j23040579 <j23040579@student.newinti.edu.my>
2026-04-27 22:14:00 +0800

    added storyboard page

--- Exercise 3/Exercise 0.2/energy_site/js/script.js
+++ Exercise 3/Exercise 0.2/energy_site/js/script.js
@@ -1,4 +1,3 @@
-
 function goHome() {
   location.href = "index.html";
 }
@@ -10,3 +9,26 @@ function goTV() {
 function goAbout() {
   location.href = "about.html";
 }
+
+function goStoryBoard() {
+  location.href = "storyboard.html";
+}
+
+const buttons = document.querySelectorAll(".toggle-btn");
+const pages = document.querySelectorAll(".page");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const pageNumber = btn.getAttribute("data-page");
+
+    // remove active from all buttons
+    buttons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    // hide all pages
+    pages.forEach((p) => p.classList.remove("active"));
+
+    // show selected page
+    document.getElementById("page" + pageNumber).classList.add("active");
+  });
+});
